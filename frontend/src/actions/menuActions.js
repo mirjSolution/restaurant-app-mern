cbf8baa32183070092759782d4eb7601939b52db
mirjSolution <ricojohndatoon23@gmail.com>
2021-03-21 10:22:29 -0400

    Menu Details Reducer And Action

--- frontend/src/actions/menuActions.js
+++ frontend/src/actions/menuActions.js
@@ -3,6 +3,9 @@ import {
   MENU_LIST_FAIL,
   MENU_LIST_SUCCESS,
   MENU_LIST_REQUEST,
+  MENU_DETAILS_REQUEST,
+  MENU_DETAILS_SUCCESS,
+  MENU_DETAILS_FAIL,
 } from '../constants/menuConstants';
 
 export const listMenus = () => async (dispatch) => {
@@ -18,6 +21,27 @@ export const listMenus = () => async (dispatch) => {
   } catch (error) {
     dispatch({
       type: MENU_LIST_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    });
+  }
+};
+
+export const listMenuDetails = (id) => async (dispatch) => {
+  try {
+    dispatch({ type: MENU_DETAILS_REQUEST });
+
+    const { data } = await axios.get(`/api/menus/${id}`);
+
+    dispatch({
+      type: MENU_DETAILS_SUCCESS,
+      payload: data,
+    });
+  } catch (error) {
+    dispatch({
+      type: MENU_DETAILS_FAIL,
       payload:
         error.response && error.response.data.message
           ? error.response.data.message
