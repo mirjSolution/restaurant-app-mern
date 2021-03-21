bd1b9dd6353a8191a9e72c27672b13fef3021a1c
mirjSolution <ricojohndatoon23@gmail.com>
2021-03-21 18:25:08 -0400

    Cart Reducer And Add To Cart Action

---
+++ frontend/src/actions/cartActions.js
@@ -0,0 +1,20 @@
+import axios from 'axios';
+import { CART_ADD_ITEM } from '../constants/cartConstant';
+
+export const addToCart = (id, order) => async (dispatch, getState) => {
+  const { data } = await axios.get(`/api/menus/${id}`);
+
+  dispatch({
+    type: CART_ADD_ITEM,
+    payload: {
+      menu: data._id,
+      name: data.name,
+      image: data.image,
+      price: data.price,
+      orderCount: data.orderCount,
+      order,
+    },
+  });
+
+  localStorage.setItem('cartItems', JSON.stringify(getState().cart.cartItems));
+};
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -2,13 +2,21 @@ import { createStore, combineReducers, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import { menuListReducer, menuDetailsReducer } from './reducers/menuReducers';
+import { cartReducer } from './reducers/cartReducers';
 
 const reducer = combineReducers({
   menuList: menuListReducer,
   menuDetails: menuDetailsReducer,
+  cart: cartReducer,
 });
 
-const initialState = {};
+const cartItemsFromStorage = localStorage.getItem('cartItems')
+  ? JSON.parse(localStorage.getItem('cartItmes'))
+  : [];
+
+const initialState = {
+  cart: { cartItems: cartItemsFromStorage },
+};
 
 const middleware = [thunk];
 
