59dcddfcb2df4691537dd1ed8369ac83917a1c57
mirjSolution <ricojohndatoon23@gmail.com>
2021-03-22 15:10:40 -0400

    Custom Authentication Middleware

--- backend/controllers/menuController.js
+++ backend/controllers/menuController.js
@@ -1,17 +1,17 @@
 import asyncHandler from 'express-async-handler';
 import Menu from '../models/menuModel.js';
 
-// @description Fetch all menus
-// @route       GET /api/menus
-// @access      Public
+// @desc    Fetch all menus
+// @route   GET /api/menus
+// @access  Public
 const getMenus = asyncHandler(async (req, res) => {
   const menus = await Menu.find({});
   res.json(menus);
 });
 
-// @description Fetch single menu
-// @route       GET /api/menus/:id
-// @access      Public
+// @desc    Fetch single menu
+// @route   GET /api/menus/:id
+// @access  Public
 
 const getMenuById = asyncHandler(async (req, res) => {
   const menu = await Menu.findById(req.params.id);
