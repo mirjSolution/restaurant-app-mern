faa3efbbbb22f8e344a4d60cee95baad19860a48
mirjSolution <ricojohndatoon23@gmail.com>
2021-03-24 18:13:58 -0400

    Admin Product List

---
+++ frontend/src/screens/MenuListScreen.js
@@ -0,0 +1,98 @@
+import React, { useEffect } from 'react';
+import { LinkContainer } from 'react-router-bootstrap';
+import { FaEdit, FaPlus, FaTrash } from 'react-icons/fa';
+import { Table, Button, Row, Col } from 'react-bootstrap';
+import { useDispatch, useSelector } from 'react-redux';
+import Message from '../components/Message';
+import Loader from '../components/Loader';
+import { listMenus } from '../actions/menuActions';
+
+const MenuListScreen = ({ history, match }) => {
+  const dispatch = useDispatch();
+
+  const menuList = useSelector((state) => state.menuList);
+  const { loading, error, menus } = menuList;
+
+  const userLogin = useSelector((state) => state.userLogin);
+  const { userInfo } = userLogin;
+
+  useEffect(() => {
+    if (userInfo && userInfo.isAdmin) {
+      dispatch(listMenus());
+    } else {
+      history.push('login');
+    }
+  }, [dispatch, history, userInfo]);
+
+  const deleteHandler = (id) => {
+    if (window.confirm('Are you sure')) {
+    }
+  };
+
+  const createProductHandler = () => {};
+
+  return (
+    <>
+      <Row className='align-items-center'>
+        <Col>
+          <h1>Menus</h1>
+        </Col>
+        <Col className='text-right'>
+          <Button
+            variant='light'
+            className='my-3 btn-outline-primary'
+            onClick={createProductHandler}
+          >
+            <FaPlus color='gold' /> Create Menu
+          </Button>
+        </Col>
+      </Row>
+
+      {loading ? (
+        <Loader />
+      ) : error ? (
+        <Message variant='danger'>{error}</Message>
+      ) : (
+        <>
+          <Table striped bordered hover responsive className='table-sm'>
+            <thead>
+              <tr>
+                <th>ID</th>
+                <th>NAME</th>
+                <th>CATEGORY</th>
+                <th>PRICE</th>
+                <th></th>
+              </tr>
+            </thead>
+            <tbody>
+              {menus.map((menu) => (
+                <tr key={menu._id}>
+                  <td>{menu._id}</td>
+                  <td>{menu.name}</td>
+                  <td>{menu.category}</td>
+                  <td>${menu.price}</td>
+                  <td className='text-center'>
+                    <LinkContainer to={`/admin/menus/${menu._id}/edit`}>
+                      <Button variant='light' className='btn-sm'>
+                        <FaEdit color='blue' />
+                      </Button>
+                    </LinkContainer>
+                    <Button
+                      variant='light'
+                      className='btn-sm'
+                      onClick={() => deleteHandler(menu._id)}
+                    >
+                      <FaTrash color='red' />
+                    </Button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </Table>
+        </>
+      )}
+    </>
+  );
+};
+
+export default MenuListScreen;
