70cb915ba7f06dcfadfd64032012d5c23125a3bf
mirjSolution <ricojohndatoon23@gmail.com>
2021-03-23 22:45:35 -0400

    Update To Endpoint

--- frontend/src/actions/orderActions.js
+++ frontend/src/actions/orderActions.js
@@ -28,7 +28,6 @@ export const createOrder = (order) => async (dispatch, getState) => {
 
     const { data } = await axios.post(`/api/orders`, order, config);
 
-    console.log(order);
     dispatch({
       type: ORDER_CREATE_SUCCESS,
       payload: data,
