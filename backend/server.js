4b0c7361792134abad43d573071a4ce8db2911af
mirjSolution <ricojohndatoon23@gmail.com>
2021-03-24 00:32:15 -0400

    Adding PayPal Payments

--- backend/server.js
+++ backend/server.js
@@ -23,8 +23,11 @@ app.use('/api/menus', menuRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/orders', orderRoutes);
 
-app.use(notFound);
+app.get('/api/config/paypal', (req, res) =>
+  res.send(process.env.PAYPAL_CLIENT_ID)
+);
 
+app.use(notFound);
 app.use(errorHandler);
 
 const PORT = process.envPORT || 5000;
