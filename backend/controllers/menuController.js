8a0b2028bb1bf05b7cb21372604b4b6d0230d2ce
mirjSolution <ricojohndatoon23@gmail.com>
2021-03-25 07:48:17 -0400

    Create and Update Product Endpoints

--- backend/controllers/menuController.js
+++ backend/controllers/menuController.js
@@ -39,4 +39,47 @@ const deleteMenu = asyncHandler(async (req, res) => {
   }
 });
 
-export { getMenuById, getMenus, deleteMenu };
+// @desc    Create a menu
+// @route   POST /api/menus
+// @access  Private/Admin
+const createMenu = asyncHandler(async (req, res) => {
+  const menu = new Menu({
+    name: 'Sample name',
+    price: 0,
+    user: req.user._id,
+    image: '/images/sample.jpg',
+    category: 'Breakfast',
+    orderStock: 0,
+    numReviews: 0,
+    description: 'Sample description',
+  });
+
+  const createdMenu = await menu.save();
+  res.status(201).json(createdMenu);
+});
+
+// @desc    Update a menu
+// @route   PUT /api/menus/:id
+// @access  Private/Admin
+const updateMenu = asyncHandler(async (req, res) => {
+  const { name, price, description, image, category, orderStock } = req.body;
+
+  const menu = await Menu.findById(req.params.id);
+
+  if (menu) {
+    menu.name = name;
+    menu.price = price;
+    menu.description = description;
+    menu.image = image;
+    menu.category = category;
+    menu.orderStock = orderStock;
+
+    const updatedMenu = await menu.save();
+    res.json(updatedMenu);
+  } else {
+    res.status(404);
+    throw new Error('Menu not found');
+  }
+});
+
+export { getMenuById, getMenus, deleteMenu, createMenu, updateMenu };
